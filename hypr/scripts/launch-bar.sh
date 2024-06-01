69d22bbfe37404baee969eef5b8a3c259066b4d5
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2024-06-01 16:20:23 +0800

    hyprland config

---
+++ hypr/scripts/launch-bar.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+swww init
+sleep 4
+# swww img ~/.config/hypr/wallpapers/background_01.jpeg 
+# swww img ~/.config/hypr/wallpapers/background_02.jpg 
+swww img ~/.config/hypr/wallpapers/background_03.jpg 
+~/.config/eww/launch_bar
